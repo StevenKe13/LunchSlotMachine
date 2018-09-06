46586c0acfabeb74a905601106c1a4f22acc9fcd
steven <steven@mishop.com.tw>
2018-09-06 14:25:57 +0800

    加入title/還沒寫全域變數

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -1,7 +1,8 @@
 <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1, maximum-scale=2.0">
 
-<title>@yield('pageName')</title>
+{{--<title>@yield('pageName')</title>--}}
+<title>米果午餐拉霸機 v3.2</title>
 
 <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/4.0.0/css/bootstrap.min.css" integrity="sha384-Gn5384xqQ1aoWXA+058RXPxPg6fy4IWvTNh0E263XmFcJlSAwiGgFAW/dAiS6JXm" crossorigin="anonymous">
 <link rel="stylesheet" href="{{ asset('/public/css/style.css') }}" type="text/css" media="screen" />
